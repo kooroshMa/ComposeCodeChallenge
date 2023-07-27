86a7eff072984781cb7f87d113d72fa34c00d643
koorosh <koorosh_mashhadi@yahoo.com>
2023-07-27 20:57:07 +0330

    added mutable state flow for handling response

--- app/src/main/java/com/example/composecodechallenge/features/userlist/viewmodel/UserListViewModel.kt
+++ app/src/main/java/com/example/composecodechallenge/features/userlist/viewmodel/UserListViewModel.kt
@@ -3,10 +3,13 @@ package com.example.composecodechallenge.features.userlist.viewmodel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.composecodechallenge.features.userlist.model.UserListItem
 import com.example.domain.model.error.Error
 import com.example.domain.model.userlist.UserModel
 import com.example.domain.usecase.GetUsersUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -15,6 +18,8 @@ class UserListViewModel @Inject constructor(
     private val getUsersUseCase: GetUsersUseCase
 ) : ViewModel(){
 
+    private val _users = MutableStateFlow(emptyList<UserListItem>())
+    val users = _users.asStateFlow()
 
     private fun getUsers(query: String) {
 
