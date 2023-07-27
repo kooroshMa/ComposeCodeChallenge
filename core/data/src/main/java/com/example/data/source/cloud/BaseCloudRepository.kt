4f20b2b4599867e691240048b0029ceb1afc13b8
koorosh <koorosh_mashhadi@yahoo.com>
2023-07-27 19:35:59 +0330

    added get user function in data source and called getUser api in cloud. Manage mock impl if u want.

--- core/data/src/main/java/com/example/data/source/cloud/BaseCloudRepository.kt
+++ core/data/src/main/java/com/example/data/source/cloud/BaseCloudRepository.kt
@@ -1,4 +1,9 @@
 package com.example.data.source.cloud
 
+import com.example.domain.model.userlist.UsersModel
+
 interface BaseCloudRepository {
+
+    suspend fun getUsers(userName:String): UsersModel
+
 }
--- core/data/src/main/java/com/example/data/source/cloud/CloudMockRepository.kt
+++ core/data/src/main/java/com/example/data/source/cloud/CloudMockRepository.kt
@@ -1,4 +1,10 @@
 package com.example.data.source.cloud
 
+import com.example.domain.model.userlist.UsersModel
+
 class CloudMockRepository: BaseCloudRepository {
+    override suspend fun getUsers(userName: String): UsersModel {
+        TODO("Not yet implemented")
+    }
+
 }
