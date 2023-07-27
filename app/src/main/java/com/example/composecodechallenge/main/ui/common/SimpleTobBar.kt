85eb24bb3ba5cea7a16a9ff7994602f856304117
koorosh <koorosh_mashhadi@yahoo.com>
2023-07-27 15:16:08 +0330

    added simple toolbar

---
+++ app/src/main/java/com/example/composecodechallenge/main/ui/common/SimpleTobBar.kt
@@ -0,0 +1,78 @@
+package com.example.composecodechallenge.main.ui.common
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.example.composecodechallenge.R
+import com.example.composecodechallenge.main.ui.theme.ThemePreview
+import com.example.composecodechallenge.main.ui.theme.space
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SimpleTopAppBar(
+    title: String,
+    onBackClick: () -> Unit
+) {
+    TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.background,
+            navigationIconContentColor = MaterialTheme.colorScheme.onBackground
+        ),
+        title = {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.headlineSmall,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = onBackClick) {
+                Icon(
+                    modifier = Modifier.size(MaterialTheme.space.xMedium),
+                    painter = painterResource(id = R.drawable.ic_back),
+                    contentDescription = "back",
+                    tint = MaterialTheme.colorScheme.onBackground
+                )
+            }
+        }
+    )
+}
+
+/**
+ * SimpleTopAppBar for our first page. We do not need navigation icon in first page
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SimpleTopAppBar(
+    title: String,
+) {
+    CenterAlignedTopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.background,
+            navigationIconContentColor = MaterialTheme.colorScheme.onBackground
+        ),
+        title = {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.headlineSmall,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        },
+    )
+}
+
+@ThemePreview
+@Composable
+fun PreviewSimpleTopBar() {
+    SimpleTopAppBar(title = "Simple toolbar") {
+    }
+}
