a293e12af6ed61dfce79a98de6a9d53b6dc6f381
koorosh <koorosh_mashhadi@yahoo.com>
2023-07-28 03:11:56 +0330

    changed user list route suffix for navigating with arg

--- app/src/main/java/com/example/composecodechallenge/features/userlist/navigation/UserListNavigation.kt
+++ app/src/main/java/com/example/composecodechallenge/features/userlist/navigation/UserListNavigation.kt
@@ -2,10 +2,11 @@ package com.example.composecodechallenge.features.userlist.navigation
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.example.composecodechallenge.features.userdetail.navigation.USER_NAME_ARG_SUFFIX
 import com.example.composecodechallenge.features.userlist.ui.UserListRoute
 
 internal const val USER_ROUTE_PREFIX = "user"
-internal const val USER_LIST_ROUTE = "${USER_ROUTE_PREFIX}_list_route"
+internal const val USER_LIST_ROUTE = "${USER_ROUTE_PREFIX}_list_route$USER_NAME_ARG_SUFFIX"
 
 fun NavGraphBuilder.userListScreen() {
     composable(route = USER_LIST_ROUTE) {
