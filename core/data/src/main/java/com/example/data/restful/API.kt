947c8047af41ff9773e3e636c87cdd9e1cf1db08
koorosh <koorosh_mashhadi@yahoo.com>
2023-07-27 19:31:27 +0330

    added query function for getting users

--- core/data/src/main/java/com/example/data/restful/API.kt
+++ core/data/src/main/java/com/example/data/restful/API.kt
@@ -1,4 +1,12 @@
 package com.example.data.restful
 
+import com.example.domain.model.userlist.UsersModel
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface API {
+
+    @GET("/search/users")
+    suspend fun getUsers(@Query("q") username: String): UsersModel
+
 }
