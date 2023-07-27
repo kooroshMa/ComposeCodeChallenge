3f5cc40e4cfeead02bcd974eb860ebd39fed124f
koorosh <koorosh_mashhadi@yahoo.com>
2023-07-28 01:14:07 +0330

    impl user detail repo

---
+++ core/data/src/main/java/com/example/data/repository/GetUserDetailRepoImpl.kt
@@ -0,0 +1,16 @@
+package com.example.data.repository
+
+import com.example.data.di.Cloud
+import com.example.data.source.cloud.BaseCloudRepository
+import com.example.domain.model.userdetail.UserDetailModel
+import com.example.domain.repository.GetUserDetailRepository
+import javax.inject.Inject
+
+class UserDetailRepoImpl @Inject constructor(
+    @Cloud private val baseCloudRepository: BaseCloudRepository
+): GetUserDetailRepository {
+
+    override suspend fun getUserDetail(userName: String): UserDetailModel {
+        return baseCloudRepository.getUserDetail(userName)
+    }
+}
