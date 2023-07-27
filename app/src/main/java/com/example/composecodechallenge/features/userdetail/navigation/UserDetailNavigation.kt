3002a6af7c11d30a6ae49255d14f1366c784116d
koorosh <koorosh_mashhadi@yahoo.com>
2023-07-28 02:55:31 +0330

    added user detail navigation considering pass argument

---
+++ app/src/main/java/com/example/composecodechallenge/features/userdetail/navigation/UserDetailNavigation.kt
@@ -0,0 +1,37 @@
+package com.example.composecodechallenge.features.userdetail.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.example.composecodechallenge.features.userdetail.ui.UserListRoute
+import com.example.composecodechallenge.features.userlist.navigation.USER_ROUTE_PREFIX
+
+private const val USER_DETAILS_ROUTE = "$USER_ROUTE_PREFIX/detail"
+const val USER_NAME = "userName"
+const val USER_NAME_ARG_SUFFIX = "/{$USER_NAME}"
+const val DEFAULT_VALUE = "Koorosh"
+
+internal fun NavController.navigateToUserDetails(
+    searchQuery: String,
+    navOptions: NavOptions? = null
+) {
+    navigate(route = "$USER_DETAILS_ROUTE/$searchQuery", navOptions = navOptions)
+}
+
+fun NavGraphBuilder.userDetailsScreen(
+    navController: NavController,
+) {
+    composable(route = USER_DETAILS_ROUTE + USER_NAME_ARG_SUFFIX,
+        arguments = listOf(navArgument(USER_NAME) {
+            type = NavType.StringType
+            defaultValue = DEFAULT_VALUE
+        })
+    ) {
+        UserListRoute(
+            onBackClick = navController::popBackStack,
+        )
+    }
+}
