0323be95c15509a45551785bf68bf069c2b3b275
koorosh <koorosh_mashhadi@yahoo.com>
2023-07-28 03:06:43 +0330

    added user graph

---
+++ app/src/main/java/com/example/composecodechallenge/features/userdetail/navigation/UserDetailGraph.kt
@@ -0,0 +1,19 @@
+package com.example.composecodechallenge.features.userdetail.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.navigation
+import com.example.composecodechallenge.features.userlist.navigation.USER_ROUTE_PREFIX
+
+const val USER_GRAPH_ROUTE_PATTERN = "user_graph"
+
+fun NavGraphBuilder.userDetailGraph(
+    navController: NavHostController
+) {
+    navigation(
+        route = USER_GRAPH_ROUTE_PATTERN,
+        startDestination = USER_ROUTE_PREFIX,
+    ) {
+        userDetailsScreen(navController)
+    }
+}
