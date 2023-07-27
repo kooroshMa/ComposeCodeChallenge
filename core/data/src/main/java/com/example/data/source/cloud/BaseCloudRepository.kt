41ca6586f3cabed7695a49f4f61e2be1babd2a45
koorosh <koorosh_mashhadi@yahoo.com>
2023-07-28 00:38:30 +0330

    added getUserDetail function in data source and called API in cloud impl. Handle mock impl if u need it

--- core/data/src/main/java/com/example/data/source/cloud/BaseCloudRepository.kt
+++ core/data/src/main/java/com/example/data/source/cloud/BaseCloudRepository.kt
@@ -1,9 +1,12 @@
 package com.example.data.source.cloud
 
+import com.example.domain.model.userdetail.UserDetailModel
 import com.example.domain.model.userlist.UsersModel
 
 interface BaseCloudRepository {
 
     suspend fun getUsers(userName:String): UsersModel
+
+    suspend fun getUserDetail(userName: String): UserDetailModel
 
 }
--- core/data/src/main/java/com/example/data/source/cloud/CloudMockRepository.kt
+++ core/data/src/main/java/com/example/data/source/cloud/CloudMockRepository.kt
@@ -1,9 +1,14 @@
 package com.example.data.source.cloud
 
+import com.example.domain.model.userdetail.UserDetailModel
 import com.example.domain.model.userlist.UsersModel
 
 class CloudMockRepository: BaseCloudRepository {
     override suspend fun getUsers(userName: String): UsersModel {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getUserDetail(userName: String): UserDetailModel {
         TODO("Not yet implemented")
     }
 
